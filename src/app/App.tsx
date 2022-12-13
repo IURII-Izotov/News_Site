ebf36c0ad410bb1f91b45023e527283a12122007
unknown <iurii.izotov@gmail.com>
2022-12-13 19:35:58 +0600

    feat:implement header layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+
 import {Header} from "../components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+        <div className='wrapper'>
+            <Header/>
+        </div>
+
     </div>
   );
 }
