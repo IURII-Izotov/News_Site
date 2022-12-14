1199474809d69d154030f5f82a49464524b54afc
unknown <iurii.izotov@gmail.com>
2022-12-14 09:16:43 +0600

    docs: add report

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {Header} from "../components/Header/Header";
 import {Footer} from "../components/Footer/Footer";
+import {RegistrationForm} from "../pages/RegistrationPage/RegistrationForm/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
         <div className='wrapper'>
+            <RegistrationForm/>
             <Header/>
             <Footer/>
         </div>
--- src/pages/RegistrationPage/RegistrationForm/RegistrationForm.tsx
+++ src/pages/RegistrationPage/RegistrationForm/RegistrationForm.tsx
@@ -1,8 +1,58 @@
+import style from './RegistrationForm.module.css'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export const RegistrationForm = () => {
-    return (
-        <div>
 
-        </div>
-    );
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+            <FormControl>
+                <FormGroup>
+                    <div className={style.formFieldWrap}>
+                        <span>Фамилия</span>
+
+                    </div>
+                    <TextField className={style.inputStyle} />
+                    {/*<TextField type="password"/>*/}
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>
+                        Login
+                    </Button>
+                </FormGroup>
+            </FormControl>
+        </Grid>
+    </Grid>
+
+
+        // <form className={style.formWrap} name="registration" method="POST" action="">
+        //     <div className={style.formFieldWrap}>
+        //         <span>Фамилия</span>
+        //         <input className={style.inputStyle} type='text' name="last_name" required={true}/>
+        //     </div>
+        //     <div className={style.formFieldWrap}>
+        //         <span>Имя</span>
+        //         <input className={style.inputStyle} type='text' name="name" required={true}/>
+        //     </div>
+        //     <div className={style.formFieldWrap}>
+        //         <span>Никнейм</span>
+        //         <input className={style.inputStyle} type='text' name="nickName" required={true}/>
+        //     </div>
+        //     <div className={style.formFieldWrap}>
+        //         <span>Пароль</span>
+        //         <input className={style.inputStyle} type="password" name="Password" required={true}/>
+        //
+        //     </div>
+        //     <span>Лимит на символы</span>
+        //     <div className={style.formFieldWrap}>
+        //         <span>Подстверждение пароля</span>
+        //         <input className={style.inputStyle} type="password" name="Password2" required={true}/>
+        //     </div>
+        //     <button type={'submit'}>Регистрация</button>
+        // </form>
+
 };
