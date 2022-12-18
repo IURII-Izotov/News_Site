3475004423744feb84446f96af7c93e50c289546
unknown <iurii.izotov@gmail.com>
2022-12-18 21:18:09 +0600

    feat:add menu purple styple

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,29 @@ import logo  from '../../assets/img/logo.svg'
 import burgerMenu  from '../../assets/icons/burger-menu.svg'
 import userIcon  from '../../assets/icons/user-icon.svg'
 import searchIcon  from '../../assets/icons/search.svg'
-export const Header:FC = () => {
+import logoPurple  from '../../assets/img/logo-purple.svg'
+import burgerMenuPurple  from '../../assets/icons/burger-menu-purple.svg'
+import userIconPurple  from '../../assets/icons/user-icon-purple.svg'
+import searchIconPurple  from '../../assets/icons/search-purple.svg'
+
+type headerPropsType={
+    subMenu?:boolean
+}
+    export const Header:FC<headerPropsType> = ({subMenu=true}) => {
     return (
-        <header className={style.header}>
+        <header className={subMenu? style.subMenu:style.header}>
             <div className={style.wrap}>
-                <img className="logo" src={logo} alt="logo"/>
-                <h1 className={style.heading}>Новости</h1>
+                <img className="logo" src={subMenu? logoPurple : logo} alt="logo"/>
+                {
+                    subMenu
+                        ? <></>
+                        : <h1 className={style.heading}>Новости</h1>
+                }
+
                 <div className={style.menuWrap}>
-                    <img className={style.icon} src={searchIcon} alt="search"/>
-                    <img className={style.icon} src={userIcon} alt="user"/>
-                    <img className={style.icon} src={burgerMenu} alt="menu"/>
+                    <img className={style.icon} src={ subMenu?searchIconPurple :searchIcon} alt="search"/>
+                    <img className={style.icon} src={ subMenu?userIconPurple :userIcon} alt="user"/>
+                    <img className={style.icon} src={ subMenu?burgerMenuPurple :burgerMenu} alt="menu"/>
                 </div>
             </div>
 
