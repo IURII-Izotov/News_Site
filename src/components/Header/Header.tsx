27775fda2e23d0bc3242f53efd6f31e34e4e5a06
unknown <iurii.izotov@gmail.com>
2023-01-05 23:10:22 +0600

    refactor:chage layout header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,19 @@ import burgerMenuPurple  from '../../assets/icons/burger-menu-purple.svg'
 import userIconPurple  from '../../assets/icons/user-icon-purple.svg'
 import searchIconPurple  from '../../assets/icons/search-purple.svg'
 import {useLocation} from 'react-router-dom'
+import {Form, Formik} from "formik";
+import image from "../../assets/icons/image.svg";
+import {baseUrl} from "../../api/user.api";
+import {ButtonIcon} from "../Button/ButtonIcon/ButtonIcon";
+import download from "../../assets/icons/download.svg";
+import trash from "../../assets/icons/trash.svg";
+import {InputComponent} from "../Input/Input";
+import edit from "../../assets/icons/edit.svg";
+import {Button} from "../Button/Button";
 
     export const Header:FC = () => {
     let [subMenu,setSubMenu] = useState(false)
+        let [showInputSearch,setShowInputSearch] =useState(true)
     let {pathname}=useLocation();
     useEffect(()=>{
         if(pathname === '/like' || pathname === '/user'){
@@ -21,22 +31,51 @@ import {useLocation} from 'react-router-dom'
 
 
     return (
-        <header className={subMenu? style.subMenu:style.header}>
+        <header className={subMenu? style.subMenu : style.header}>
             <div className={style.wrap}>
                 <img className="logo" src={subMenu? logoPurple : logo} alt="logo"/>
-                {
-                    subMenu
-                        ? <></>
-                        : <h1 className={style.heading}>Новости</h1>
-                }
-
                 <div className={style.menuWrap}>
-                    <img className={style.icon} src={ subMenu?searchIconPurple :searchIcon} alt="search"/>
+                    <Formik
+                        initialValues={
+                            {
+                            }
+                        }
+                        onSubmit={(values, {setSubmitting}) => {
+                            console.log(values)
+
+                            setSubmitting(false);
+                        }}
+                    >
+                        {({isSubmitting,
+                              errors, touched, values, handleChange}) => (
+                            <div className={style.formContainer}>
+                                <Form className={style.formContainer}>
+                                    <div className={style.formWrap}>
+                                        <div className="formFieldWrap">
+                                            <div className="inputContainer">
+                                                <InputComponent
+                                                    type="text" nameField="search_text"
+                                                    errors={errors}
+                                                    isSubmitting={isSubmitting}
+                                                hidden={showInputSearch}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Form>
+                            </div>
+                        )}
+                    </Formik>
+
+                    <img onClick={()=>setShowInputSearch(showInputSearch)} className={style.icon} src={ subMenu?searchIconPurple :searchIcon} alt="search"/>
                     <img className={style.icon} src={ subMenu?userIconPurple :userIcon} alt="user"/>
                     <img className={style.icon} src={ subMenu?burgerMenuPurple :burgerMenu} alt="menu"/>
                 </div>
             </div>
-
+            {
+                subMenu
+                    ? <></>
+                    : <h1 className={style.heading}>Новости</h1>
+            }
         </header>
     );
 };
