f87ae6e33cf6bac15ff9517714f8650bf902953d
unknown <iurii.izotov@gmail.com>
2023-01-09 02:55:43 +0600

    feat:add validation for authorization

--- src/pages/RegistrationPage/RegistrationForm/RegistrationForm.tsx
+++ src/pages/RegistrationPage/RegistrationForm/RegistrationForm.tsx
@@ -1,32 +1,42 @@
 import style from './RegistrationForm.module.css'
 import logo from "../../../assets/img/logo-purple.svg";
-import {Formik,Form} from 'formik';
+import {Formik, Form} from 'formik';
 import {InputComponent} from "../../../components/Input/Input";
 import {Button} from "../../../components/Button/Button";
 import {useEffect} from "react";
 import {QueryStatus} from "@reduxjs/toolkit/query";
 import {useCreateRegistrationMutation} from '../../../api/login.api'
 import {Link} from "react-router-dom";
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+    password: Yup.string()
+        .min(4, 'Too Short!')
+        .max(8, 'Too Long!')
+        .required('Лимит на символы'),
+});
+
 
 export const RegistrationForm = () => {
     const [createRegistration, res] = useCreateRegistrationMutation();
     console.log(res);
-    useEffect(()=>{
-        if(res?.status == QueryStatus.fulfilled){
+    useEffect(() => {
+        if (res?.status == QueryStatus.fulfilled) {
             localStorage.setItem('nickname', res.data.nickname);
             if (typeof window !== 'undefined') {
                 const win: Window = window;
                 win.location = '/login';
             }
         }
-    },[res])
-    let handleClickLogin = ()=>{
+    }, [res])
+    let handleClickLogin = () => {
         localStorage.setItem('nickname', 'undefined');
         if (typeof window !== 'undefined') {
             const win: Window = window;
             win.location = '/login';
         }
     }
+
     return (
         <Formik
             initialValues={
@@ -34,30 +44,31 @@ export const RegistrationForm = () => {
                     last_name: '',
                     name: '',
                     nickname: '',
-                    profile_image:null,
+                    profile_image: null,
                     password: '',
                     password2: ''
                 }
             }
-            onSubmit={(values, { setSubmitting }) => {
+            validationSchema={SignupSchema}
+            onSubmit={(values, {setSubmitting}) => {
+                console.log(values)
                 createRegistration(values);
-             console.log(values)
                 setSubmitting(false);
             }}
         >
-            {({ isSubmitting,errors,touched,values,handleChange }) => (
+            {({isSubmitting, errors, touched, values, handleChange}) => (
                 <div className={style.formContainer}>
                     <img className={style.logo} src={logo} alt=""/>
                     <Form className={style.formWrap}>
                         <div className="formFieldWrap">
                             <label htmlFor="last_name">Фамилия</label>
                             <div className="inputContainer">
-                                <InputComponent  onChange={handleChange}
-                                                 value={values.last_name}
-                                                 type="text" nameField="last_name"
-                                                 errors={errors}
-                                                 touched ={touched}
-                                                 isSubmitting={isSubmitting} />
+                                <InputComponent onChange={handleChange}
+                                                value={values.last_name}
+                                                type="text" nameField="last_name"
+                                                errors={errors.last_name}
+                                                touched={touched}
+                                                isSubmitting={isSubmitting}/>
                             </div>
                         </div>
                         <div className="formFieldWrap">
@@ -67,8 +78,8 @@ export const RegistrationForm = () => {
                                                 value={values.name}
                                                 type="text"
                                                 nameField="name"
-                                                errors={errors}
-                                                touched ={touched}/>
+                                                errors={errors.name}
+                                                touched={touched}/>
                             </div>
                         </div>
                         <div className="formFieldWrap">
@@ -78,8 +89,8 @@ export const RegistrationForm = () => {
                                                 value={values.nickname}
                                                 type="text"
                                                 nameField="nickname"
-                                                errors={errors}
-                                                touched ={touched}/>
+                                                errors={errors.nickname}
+                                                touched={touched}/>
                             </div>
                         </div>
                         <div className="formFieldWrap">
@@ -90,29 +101,38 @@ export const RegistrationForm = () => {
                                                     value={values.password}
                                                     type="password"
                                                     nameField="password"
-                                                    errors={errors}
-                                                    touched ={touched}/>
+                                                    errors={errors.password}
+                                                    touched={touched}/>
                                 </div>
-                                <span className={style.subSpan}>Лимит на символы</span>
+                                <span
+                                    className={style.subSpan}>{errors.password ? errors.password : "Лимит на символы"}</span>
                             </div>
 
                         </div>
                         <div className="formFieldWrap">
+
                             <label htmlFor="password2">Подтверждение пароля</label>
-                            <div className="inputContainer">
-                                <InputComponent onChange={handleChange}
-                                                value={values.password2}
-                                                type="password"
-                                                nameField="password2"
-                                                errors={errors}
-                                                touched ={touched} />
+                            <div className="formFieldWrapNotification">
+                                <div className="inputContainer">
+                                    <InputComponent onChange={handleChange}
+                                                    value={values.password2}
+                                                    type="password"
+                                                    nameField="password2"
+                                                    errors={errors.password2}
+                                                    touched={touched}/>
+                                </div>
+                                <span className={style.subSpan}>{
+                                    // @ts-ignore
+                                    res?.error?.data?.password ? 'Пароли дожны совпадать' : ''
+                                }</span>
                             </div>
                         </div>
                         <div className={style.buttonContainer}>
                             <Button type="submit" text="Регистрация" disabled={isSubmitting}/>
                         </div>
                     </Form>
-                    <span  className={style.textLogin}>Уже есть логин? <Link onClick={()=>handleClickLogin()} to={'/login'}>Войти</Link></span>
+                    <span className={style.textLogin}>Уже есть логин? <Link onClick={() => handleClickLogin()}
+                                                                            to={'/login'}>Войти</Link></span>
                 </div>
             )}
         </Formik>
