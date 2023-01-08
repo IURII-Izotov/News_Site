a69677b0c3fb89536cfa516544bb9a78e16710a3
unknown <iurii.izotov@gmail.com>
2023-01-09 01:18:36 +0600

    feat:add links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import style from './Footer.module.css'
 import {FC} from "react";
 import logo from "../../assets/img/logo.svg";
+import {Link} from "react-router-dom";
 
 export const Footer:FC = () => {
     return (
@@ -8,8 +9,8 @@ export const Footer:FC = () => {
             <div className={style.wrap}>
                 <img className={`logo ${style.logo}`} src={logo} alt="logo"/>
                 <div className={style.wrapMenu}>
-                    <a className={style.link}>Мой профиль</a>
-                    <a className={style.link}>Избранные новости</a>
+                    <Link to={'/user'} className={style.link}>Мой профиль</Link>
+                    <Link to={'/like'} className={style.link}>Избранные новости</Link>
                 </div>
             </div>
 
