e8c815c6b5612c74c3f377e3afda5885f705e2e9
unknown <iurii.izotov@gmail.com>
2023-01-17 22:59:31 +0600

    refactor: css styles

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -10,31 +10,36 @@ import {useSetReplayMutation} from '../../api/post.api'
 type commentProps = {
     isReplay?: boolean
     comment?: CommentType
-    replay?:ReplayType
-    postID?:string
-    parentID?:number
+    replay?: ReplayType
+    postID?: string
+    parentID?: number
 }
-export const Comment: FC<commentProps> = ({isReplay = false,
+export const Comment: FC<commentProps> = ({
+                                              isReplay = false,
                                               comment,
                                               replay,
                                               postID,
-                                          parentID}) => {
-    let [inputVisible,setInputVisible]=useState(false);
+                                              parentID
+                                          }) => {
+    let [inputVisible, setInputVisible] = useState(false);
 
-    let [setReplay,res]=useSetReplayMutation();
+    let [setReplay, res] = useSetReplayMutation();
     return (
         <div className={isReplay ? `${style.replayContainer} ${style.commenContainer}` : style.commenContainer}>
             <h3 className={style.nickName}>{isReplay ? replay?.user.nickname : comment?.user.nickname}</h3>
-            <p className={style.textComment}>{isReplay ? replay?.text :comment?.text}</p>
+            <p className={style.textComment}>{isReplay ? replay?.text : comment?.text}</p>
             <div className={style.footerComment}>
                 <span className={style.dateComment}>30.11.2022</span>
-                <button type={'button'}  className={style.replyComment} onClick={()=>setInputVisible(!inputVisible)}  >Ответить</button>
+                <button type={'button'} className={style.replyComment}
+                        onClick={() => setInputVisible(!inputVisible)}>Ответить
+                </button>
             </div>
 
             <Formik
                 initialValues={
-                    {   post:Number(postID),
-                        text:'',
+                    {
+                        post: Number(postID),
+                        text: '',
                         parent: parentID
                     }
                 }
@@ -43,24 +48,27 @@ export const Comment: FC<commentProps> = ({isReplay = false,
                     setSubmitting(false);
                 }}
             >
-                {({isSubmitting, errors, touched,setFieldValue}) => (
-                    <Form className={style.formComment} >
+                {({isSubmitting, errors, touched, setFieldValue}) => (
+                    <Form className={style.formComment}>
                         {inputVisible
-                            ? (<>
+                            ? (<div className={style.replayFormContainer}>
                                     <label className={style.labelStyle} htmlFor="text">Вы</label>
-                                    <Textarea setFieldValue ={setFieldValue }
-                                              isReplay={true}
-                                              type="text"
-                                              nameField="text"
-                                              errors={errors} touched={touched}/>
+                                    <div className={style.textareaContainer}>
+                                        <Textarea setFieldValue={setFieldValue}
+                                                  isReplay={true}
+                                                  type="text"
+                                                  nameField="text"
+                                                  errors={errors} touched={touched}/>
+                                    </div>
                                     <div className={style.buttonContainer}>
                                         <Button type={'submit'} text={'Ответить'} disabled={isSubmitting}/>
                                     </div>
 
-                                </>
-                               )
 
-                            :<></>
+                                </div>
+                            )
+
+                            : <></>
                         }
 
                     </Form>
